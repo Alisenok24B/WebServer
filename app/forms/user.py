82257b483cae6a7b2ffa272398fcb795ca84eac0
Alice <alisabikson@gmail.com>
2021-04-22 17:03:37 +0300

    correct deficiencie

--- app/forms/user.py
+++ app/forms/user.py
@@ -7,7 +7,7 @@ class RegisterForm(FlaskForm):  # register form
     name = StringField('Имя пользователя', validators=[DataRequired()])  # name
     password = PasswordField('Пароль', validators=[DataRequired()])  # password
     password_again = PasswordField('Повторите пароль', validators=[DataRequired()])  # password again
-    date = DateField('Дата рождения', format='%d.%m.%Y')  # date of birth
+    date = DateField('Дата рождения', format='%d.%m.%Y', validators=[DataRequired()])  # date of birth
     submit = SubmitField('Зарегистрироваться')  # button register
 
 

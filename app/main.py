74d1843e6d9e79ddc6e60c5e35cc5b1b6d249388
Alice <alisabikson@gmail.com>
2021-04-23 17:35:12 +0300

    add comments

--- app/main.py
+++ app/main.py
@@ -1,5 +1,4 @@
-from os import abort
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 from data import db_session
 from data.users import User
 from forms.user import RegisterForm, LoginForm

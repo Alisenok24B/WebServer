df118e6ae22cd94d1af476c571d41ab8cdeb93ef
Alice <alisabikson@gmail.com>
2021-04-23 18:11:55 +0300

    clear code

--- app/forms/user.py
+++ app/forms/user.py
@@ -1,13 +1,16 @@
 from flask_wtf import FlaskForm
-from wtforms import PasswordField, StringField, SubmitField, BooleanField, DateField
+from wtforms import PasswordField, StringField, SubmitField,\
+    BooleanField, DateField
 from wtforms.validators import DataRequired
 
 
 class RegisterForm(FlaskForm):  # register form
     name = StringField('Имя пользователя', validators=[DataRequired()])  # name
     password = PasswordField('Пароль', validators=[DataRequired()])  # password
-    password_again = PasswordField('Повторите пароль', validators=[DataRequired()])  # password again
-    date = DateField('Дата рождения', format='%d.%m.%Y', validators=[DataRequired()])  # date of birth
+    password_again = PasswordField(
+        'Повторите пароль', validators=[DataRequired()])  # password again
+    date = DateField('Дата рождения', format='%d.%m.%Y',
+                     validators=[DataRequired()])  # date of birth
     submit = SubmitField('Зарегистрироваться')  # button register
 
 
